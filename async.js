cb522c041ddf37fdacf06c7f8aab1c0153dd5cbd
James Litherland <jamilth@gmail.com>
2017-09-03 22:20:58 -0400

    progress (finally)

---
+++ async.js
@@ -0,0 +1,10 @@
+/*
+ *  (ASYNC.js):
+ *      this - Anattempt at understanding what this means.
+ *
+ *-------------------------------------->8------------------------------------*/
+
+
+//'use strict';
+
+var request = require('request');
--- bamazon-customer.js
+++ bamazon-customer.js
@@ -1,125 +1,85 @@
 /*
  *  (bamazon):
- *      bamazon-customer.js -list the products table, and allow the user to
- *                 order those that he likes.
+ *      bamazon-customer.js - list the products, and allow the user to order
+ *                  as he sees fit.
  *
  *-------------------------------------->8------------------------------------*/
 
-var mysql = require('mysql')
-  , clc = require('cli-color')
-  , inquirer = require('inquirer');
+var mysql = require('mysql'),
+    clc = require('cli-color'),
+    inquirer = require('inquirer');
 
-var creds = {
-  uname: null,
-  pass: null
+var sql_info = {
+    user: "james",
+    password: "james",
+    host: "localhost",
+    database: "bamazon"
 };
 
-var inventory_results = [];
-var item_id_list = [];
-var logarr = [];
+var products = [],
+    ids = [];
 
-function logArray(arr) {
-  if (arr.length < 1) {
-    return false;
-  } else {
-    for (var i = 0; i < arr.length; i++) {
-      console.log(arr[i]);
-    }
-  }
+function getTable() {
+    var cxs = mysql.createConnection(sql_info);
+    cxs.connect();
+    var query = "SELECT * FROM `products`";
+    cxs.query(query, (err, resp, vals) => {
+        if (!err) {
+            products = JSON.parse(JSON.stringify(resp));
+            printTable();
+        } else {
+            throw Error(err);
+        }
+    });
+    cxs.end();
 }
-function list_items() {
-  var connection = mysql.createConnection({
-    host: 'localhost',
-    user: creds.uname,
-    password: creds.pass,
-    database: 'bamazon'
-  });
-  connection.connect();
-  connection.query('SELECT * FROM`products`', function (err, results, vals) {
-    if (! err) {
-      inventory_results += results;
+var printTable = function() {
+    for (var i = 0; i < products.length; i++) {
+        console.log(clc.cyan('#' + products[i].item_id + ': ') + clc.green(products[i].department_name + ' > ') + clc.red(products[i].product_name));
+        console.log(clc.red('> ') + clc.white('Only ' + clc.green(products[i].stock_qty) + ' left @ ') + clc.green(products[i].price));
+        ids += [ products[i].item_id ];
+    }
+    submitOrder();
+};
+var submitOrder = function() {
+    var questions = [
+        { type: "input",
+          name: "id",
+          message: "Enter the ID of the item you'd like to order." },
+        { type: "input",
+          name: "qty",
+          message: "Quantity to order?" }
+    ];
+    inquirer.prompt(questions).then(function(answers) {
+        if (ids.indexOf(answers.id) !== -1 && answers.qty > 0) {
+            if (products[answers.id].stock_qty >= answers.qty) {
+                console.log('alls wel;');
+                var qty = products[answers.id].stock_qty - answers.qty;
+                console.log('qty: ' + qty);
+                updateTable(answers.id, qty);
+            }
+            else {
+                console.log("We don't have enough stock to fill your order... please try again.");
+                printTable();
+            }
+        }
+    });
+};
+function updateTable(id, qty) {
+    var cxs = mysql.createConnection(sql_info);
+    cxs.connect();
+    var query = "UPDATE `bamazon`.`products` SET `stock_qty`='" + qty + "' WHERE `item_id`='" + id + "';";
+    console.log(query);
+    cxs.query(query, (err, resp, vals) => {
+        if (!err) {
+            console.log('Order complete!');
+            getTable();
+        }
+        else {
+            throw Error(err);
+        }
+    });
+}
+getTable();
 
-      item_id_list += [ results[i].item_id ];
-      for (var i = 0; i < results.length; i++) {
-        logarr += [ i + ":  ID: " + results[i].item_id ];
-        logarr += [ ' > ' + results[i].department_name + ' > ' + results[i].product_name ];
-        logarr += [ ' > Only ' + results[i].stock_qty + ' left @ $' + results[i].price ];
-      }
-      connection.end();
-      rcvOrder();
-    }
-    else {
-      connection.end();
-      throw Error('Connection failed.')
-    }
-  });
-}
-function get_by_id(id) {
-  console.log('get_by_id()...');
-  if (creds.uname === null || creds.pass === null) creds = getCreds();
-  var cxs = mysql.createConnection({
-    host: 'localhost',
-    user: creds.uname,
-    password: creds.pw,
-    database: "bamazon"
-  });
-  cxs.connect();
-  var SQL_str = 'SELECT * FROM `products` WHERE `item_id` = ' + id;
-  console.log('SQL query: ' + SQL_qry);
-  cxs.query(SQL_str, function (err, resp, vals) {
-    console.log('resp recvd...');
-    if (! err) {
-      if (resp.length == 1) {
-        console.log('SQL response was everything I could hope for...');
-        return resp;
-      } else if (resp.length > 1) {
-        throw Error('Multiple records were returned with the same UNIQUE item id.');
-      } else {
-        throw Error('No results for the Item ID you supplied (which seemed fine earlier?');
-      }
-    }
-  });
-}
-function rcvOrder() {
-  var questions = [
-  { type: "input",
-    name: "id",
-    message: "The Item ID you'd like to order?" },
-  { type: "input",
-    name: "qty",
-    message: "Quantity to order?",
-    default: 1 }
-  ];
-  inquirer.prompt(questions).then(function (answers) {
-    if (item_id_list.indexOf(answers.id) !== -1 && answers.qty > 0) {
-      var target_item = get_by_id(answers.id);
-    } else {
-      console.log('Invalid input... try again.');
-      list_items();
-    }
-  });
-}
-function getCreds(next) {
-  if (creds.uname !== null) next();
-  creds.uname = 'james';
-  var init_questions = [
-  { type: "input",
-    name: "user",
-    message: "Username:",
-    default: creds.uname },
-  { type: "password",
-    name: "db_pw",
-    message: "Password:" }
-  ];
-  inquirer.prompt(init_questions).then(function(resp) {
-    var userCreds = {
-      uname: resp.user,
-      pass: resp.db_pw
-    };
-    next();
-  });
-}
-getCreds(function() {
-  console.log(`Got creds... calling list_items`);
-  list_items();
-});
+
